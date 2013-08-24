8d0da22acc871160886d0338f1130fe0e937ee0c
Ashwith Jerome Rego <ashwith@gmail.com>
2013-08-24 13:51:01 +0530

    Added licenses to the code files

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,19 @@
-#!/bin/python
+#
+#    Copyright (c) 2013 Ashwith Jerome Rego
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # There are two base cases for this function.
 # We know this because the series is defined
 # with the first two values being 0 and 1.
