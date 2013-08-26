28adfc143ed0fd916a59cdc293e05b1b4428e37e
Ashwith Jerome Rego <ashwith@gmail.com>
2013-08-26 21:52:27 +0530

    Initial Version for radioactive.py - Radioactive decay Monte Carlo Simulation

---
+++ python/code/radioactivity.py
@@ -0,0 +1,72 @@
+#!/bin/python
+import pylab
+import random
+import math
+
+#=================================================================
+#          Radioactive Decay - Monte Carlo Simulation            #
+#=================================================================
+
+
+def simRadio(nMolecules, halfLife, nTrials, tStop, tStep):
+    """
+    Performs a Monte Carlo Simulation for radioactive decay
+
+    Keyword arguments:
+    nMolecules -- number of molecules in the material
+    halfLife   -- Half life of the material in time units
+    nTrials    -- Number of simulation trials to be performed
+    tStop      -- Stop time for each simulation in time units
+    tStep      -- Step time in simulation for each time unit.
+
+    Return a list of time coordinates and a list of the average
+    number of molecules at each time step.
+    """
+
+    # Calculate the decay constants,
+    # the number of simulation steps
+    k = math.log(2)/halfLife
+    nSteps = int(tStop/tStep)
+
+    # initialize list to store average number
+    # of molecules after each simulations step
+    nMol = [0] * nSteps
+
+    # First loop - runs through each trial
+    for index_i in range(nTrials):
+        tmpNMolecules = nMolecules
+
+        # Seond loop - runs through each simulations step
+        for index_j in range(nSteps):
+
+            # Third loop - decide the fate of each molecule
+            # in a simulation step. Delete it randomly
+            # probability k
+            for index_k in range(tmpNMolecules):
+                if random.uniform(0,1) <= k*tStep:
+                    tmpNMolecules -= 1
+            nMol[index_j] += tmpNMolecules
+    
+    tAxis = []
+    # Calculate the average for each simulation step
+    # and the coordinates for the time axis
+    for index_i in range(nSteps):
+        nMol[index_i] /= float(nTrials)
+        tAxis += [index_i*tStep]
+
+    return [0] + tAxis, [nMolecules] + nMol
+
+def testSim():
+    nMolecules = 100
+    nTrials = 1000
+    tStop = 15
+    tStep = 0.1
+    halfLife = 2
+    pylab.figure();
+    t, y =  simRadio(nMolecules, halfLife, nTrials, tStop, tStep)
+    pylab.plot(t, y)
+    pylab.title("Radioactive Decay - Monte Carlo Simulation")
+    pylab.xlabel("Time")
+    pylab.ylabel("Number of Molecules")
+    pylab.text(6, 90, "Simulated Half Life = " + str(y[int(2/tStep)]) + " time units.")
+    pylab.show()
